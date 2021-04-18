4f8d17e0bfabfa620a424dec8f16623e0da5cbf1
Elena Churilov <elena.churilov@gmail.com>
2021-04-18 11:44:04 -0700

    Define index route

--- collector.py
+++ collector.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 import sqlite3
 import time
 
@@ -18,30 +18,3 @@ def index():
 		output = output + str(row) + "\n"
 	con.close()
 	return output
-
-@app.route("/new", methods=['POST'])
-def create():
-	received = request.date or time.time()
-	con = sqlite3.connect("metrics.db")
-	cursor = con.cursor()
-	metrics = request.get_json()
-	for chip in metrics.keys():
-		for label in metrics[chip].keys():
-			cursor.execute(f"INSERT INTO metrics VALUES (?, ?, ?, ?)",(label, chip, received, metrics[chip][label]))
-	con.commit()
-	con.close()
-	return ("",200)
-
-@app.route("/show", methods=['GET'])
-def show():
-	con = sqlite3.connect("metrics.db")
-	cursor = con.cursor()
-	response = []
-	start = request.args.get("start", 0) 
-	stop = request.args.get("stop", time.time())
-	for row in cursor.execute(f"SELECT * FROM metrics WHERE received >=? AND received <=? ORDER BY received ASC",(start, stop)):
-		response.append({"label": row[0],"chip": row[1],"received": row[2], "value": row[3]})
-	con.commit()
-	con.close()
-	return jsonify(response)
-	
