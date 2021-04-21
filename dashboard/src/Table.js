b5ed5e9694477d05ec1bcc4373ac07916e137b03
Elena Churilov <elena.churilov@gmail.com>
2021-04-21 16:02:18 -0700

    Create form to get data from db

--- dashboard/src/Table.js
+++ dashboard/src/Table.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
+
+let db = "http://localhost:5000"
+
+async function getData(event) {
+  let form = new FormData(event.target.form)
+  let cleanForm = Array.from(form).filter(([_,v]) => v!=="")  
+  let params = new URLSearchParams(cleanForm)
+  await fetch(`${db}/show?${params}`)
+}
+
 function Table() {
   return (
     <div className="Table">
-      <table><tr><td dangerouslySetInnerHTML={svg}/></tr></table>
+      <form>
+        <fieldset>
+          <label>Chip<input name="chip" onChange={getData} /></label>
+          <label>Label<input name="label" onChange={getData}/></label>
+          <label>Start<input type="datetime-local" name="start" onChange={getData}/></label>
+          <label>End<input type="datetime-local" name="end" onChange={getData}/></label>
+        </fieldset>
+      </form>
+      <table><tr><td></td></tr></table>
     </div>
   );
 }
