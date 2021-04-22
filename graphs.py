1bc97b2eba869dbb7ce8cb71c01bfb25fde1a34c
Elena Churilov <elena.churilov@gmail.com>
2021-04-21 17:29:11 -0700

    Format graph

--- graphs.py
+++ graphs.py
@@ -12,9 +12,13 @@ def graph():
 		line = request.args[name].split(';')
 		points = [point.split(',') for point in line]
 		ax.plot(
-			[point[0] for point in points],
+			np.array([point[0] for point in points],dtype=np.datetime64),
 			[point[1] for point in points]
 		)
+	ax.set_xlabel('Time')
+	fig.autofmt_xdate()
+	ax.set_title('Data')
+	ax.grid(True)
 	output = StringIO()
 	fig.savefig(output,format="svg")
 	output.seek(0)
